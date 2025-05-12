cb31eac971c408ab562f6456feb244aba0cf6a07
Ahsan Tonmoy <ahosant82@gmail.com>
2025-05-13 00:50:47 +0600

    add tesk-5

---
+++ src/tesk-5.ts
@@ -0,0 +1,10 @@
+function isLeapYear(year: number): boolean {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+}
+
+const year = 2024;
+if (isLeapYear(year)) {
+  console.log(`${year} is a leap year. Happy New Year!`);
+} else {
+  console.log(`${year} is not a leap year. Happy New Year!`);
+}
